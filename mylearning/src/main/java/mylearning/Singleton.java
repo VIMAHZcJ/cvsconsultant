5ecd913ed797f2072e7e11fe02ddc53f2c85e921
Vimal Aggarwal <pariaggarwal@vimals-mbp.attlocal.net>
2018-03-03 22:52:39 -0600

    Singleton Java Pattern

--- mylearning/src/main/java/mylearning/Singleton.java
+++ mylearning/src/main/java/mylearning/Singleton.java
@@ -1,10 +1,26 @@
 package mylearning;
 
-public class Singleton {
+public class Singleton { // Lazy initialization
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static final Singleton instance = new Singleton();
+//	private static boolean isFirstThread = true;
+
+	private Singleton() { // This class cannot be initiated/ Defeat Initialization.
 
 	}
+
+	/*
+	 * public static Singleton getInstanceWithMultiThreading() {
+	 * 
+	 * if (instance == null) { synchronized (Singleton.class) { doSomeActivity();
+	 * instance = new Singleton(); }
+	 * 
+	 * } return instance; }
+	 * 
+	 * public static void doSomeActivity() { try { if (isFirstThread) {
+	 * isFirstThread = false; System.out.println("Thread is sleeping");
+	 * Thread.sleep(5000); } } catch (InterruptedException e) { // TODO
+	 * Auto-generated catch blocks e.printStackTrace(); } }
+	 */
 
 }
